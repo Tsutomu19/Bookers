97745f04496d60139411fbfea7849aff5e83e7bd
竹田勤 <takedatsutomu@taketakinnoMacBook-puro.local>
2020-02-10 20:25:38 +0900

    [Add]CSS追加＿前

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,12 @@
 class BooksController < ApplicationController
   def index
-     @book = Book.new
-     @books = Book.all
+      @book = Book.new
+      @books = Book.all
 
   end
 
   def show
-    @book = Book.find(params[:id])
+      @book = Book.find(params[:id])
 
   end
 
@@ -15,13 +15,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+      book = Book.new(book_params)
+      book.save
+      redirect_to book_path(book.id)
   end
 
   def edit
-    @book = Book.find(params[:id])
+      @book = Book.find(params[:id])
   end
 
   def update
@@ -31,9 +31,9 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+      book = Book.find(params[:id])
+      book.destroy
+      redirect_to books_path
   end
 
 
